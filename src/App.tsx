84423591f947c6b1633b0eb3a02de83d8fe5141a
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2025-04-27 21:54:36 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="text-5xl font-bold">Project Antares</div>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
