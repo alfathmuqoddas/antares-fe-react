4894c9e059b1756cb11edaa43cefa03deb138a22
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2025-05-16 21:54:27 +0700

    update: update index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
 
 function App() {
   return (
     <>
-      <div className="text-5xl font-bold">Project Antares</div>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <Button>Click me</Button>
+      <div className="flex justify-center items-center min-h-[70vh]">
+        <div>
+          <div className="mb-8">
+            <h1 className="text-5xl font-bold">Project Antares</h1>
+          </div>
+          <div className="flex items-center">
+            <input
+              type="text"
+              placeholder="Search for movies or theaters"
+              className="w-full bg-white p-4 rounded-l-2xl hover:shadow"
+            />
+            <div className="p-4 bg-blue-500 text-white rounded-r-2xl">
+              <Search />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
